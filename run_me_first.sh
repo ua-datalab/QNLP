430444b9e66e67ae94108195c704bff810eb671d
mithunpaul08 <mithunpaul08@gmail.com>
2024-11-05 14:07:43 -0700

    creating installation from shell script

---
+++ run_me_first.sh
@@ -0,0 +1,9 @@
+python3 -m pip install --upgrade pip
+pip install scipy 
+pip install numpy   
+pip install spacy     
+pip install tensorflow
+pip install lambeq
+pip install fasttext
+python3 -m spacy download es_core_news_sm
+python3 -m spacy download en_core_web_sm
