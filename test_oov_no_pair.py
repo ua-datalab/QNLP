74d3cf3734005a272708540dc784451ebd17920b
mithunpaul08 <mithunpaul08@gmail.com>
2024-11-16 23:06:01 -0700

    add test cases+ passes

--- test_oov_no_pair.py
+++ test_oov_no_pair.py
@@ -3,18 +3,8 @@ import pytest
 import OOV_classification_no_pair_sents
 from OOV_classification_no_pair_sents import run_experiment
 
-class TestClass:
-    def test_one(self):
-        x = "this"
-        assert "h" in x
 
-    def test_two(self):
-        x = "hello"
-        assert hasattr(x, "check")
-
-    def test_mytest():
-        with pytest.raises(SystemExit):
-            
+def test_run_expt():            
             smart_loss, smart_acc= run_experiment(nlayers=3, seed=2)
             assert smart_loss >= 0.695 
             assert smart_acc >= 0.433
