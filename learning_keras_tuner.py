fca3f9286e50e6411f77318576d4511525cba94f
mithunpaul08 <mithunpaul08@gmail.com>
2024-11-21 12:00:52 -0700

    keras learner has runner also now

--- learning_keras_tuner.py
+++ learning_keras_tuner.py
@@ -1,24 +1,67 @@
+import numpy as np
 import keras_tuner
 import keras
 from keras import layers
 
 
-def build_model(hp):
+def call_existing_code(units, activation, dropout, lr):
     model = keras.Sequential()
     model.add(layers.Flatten())
-    model.add(
-        layers.Dense(
-            # Define the hyperparameter.
-            units=hp.Int("units", min_value=32, max_value=512, step=32),
-            activation="relu",
-        )
-    )
+    model.add(layers.Dense(units=units, activation=activation))
+    if dropout:
+        model.add(layers.Dropout(rate=0.25))
     model.add(layers.Dense(10, activation="softmax"))
     model.compile(
-        optimizer="adam",
+        optimizer=keras.optimizers.Adam(learning_rate=lr),
         loss="categorical_crossentropy",
         metrics=["accuracy"],
     )
     return model
 
+
+def build_model(hp):
+    units = hp.Int("units", min_value=32, max_value=512, step=32)
+    activation = hp.Choice("activation", ["relu", "tanh"])
+    dropout = hp.Boolean("dropout")
+    lr = hp.Float("lr", min_value=1e-4, max_value=1e-2, sampling="log")
+    # call existing model-building code with the hyperparameter values.
+    model = call_existing_code(
+        units=units, activation=activation, dropout=dropout, lr=lr
+    )
+    return model
+
+
 build_model(keras_tuner.HyperParameters())
+
+tuner = keras_tuner.RandomSearch(
+    hypermodel=build_model,
+    objective="val_accuracy",
+    max_trials=3,
+    executions_per_trial=2,
+    overwrite=True,
+    directory="output",
+    project_name="mnist",
+)
+
+
+(x, y), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+x_train = x[:-10000]
+x_val = x[-10000:]
+y_train = y[:-10000]
+y_val = y[-10000:]
+
+x_train = np.expand_dims(x_train, -1).astype("float32") / 255.0
+x_val = np.expand_dims(x_val, -1).astype("float32") / 255.0
+x_test = np.expand_dims(x_test, -1).astype("float32") / 255.0
+
+num_classes = 10
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_val = keras.utils.to_categorical(y_val, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+tuner.search(x_train, y_train, epochs=2, validation_data=(x_val, y_val))
+
+models = tuner.get_best_models(num_models=2)
+best_model = models[0]
+best_model.summary()
