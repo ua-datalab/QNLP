f659b005a4c2740002789acccab5b27a4ad0c59d
mithunpaul08 <mithunpaul08@gmail.com>
2024-12-10 06:17:24 -0700

    passing val data points also as a command line argument

--- test_oov_no_pair.py
+++ test_oov_no_pair.py
@@ -12,6 +12,8 @@ import classify
 #         assert round(smart_acc,1)  >= 0.8 
 #         assert round(smart_acc,1)  <= 0.9 
 
+
+#for a combination of train=20,val=10,test=10
 def test_sst2_classical1(monkeypatch):        
         monkeypatch.setattr(sys, 'argv', 
                             ['classify.py', 
@@ -21,7 +23,9 @@ def test_sst2_classical1(monkeypatch):
                              '--model14type', 'PytorchModel',
                              '--trainer', 'PytorchTrainer',
                              '--epochs_train_model1', '7',
-                             '--no_of_training_data_points_to_use', '20'
+                             '--no_of_training_data_points_to_use', '20',
+                             '--no_of_val_data_points_to_use', '10'
+                             
                              ])
         model4_loss, model4_acc=classify.main()                         
         assert round(model4_loss,2)  >= 0.6
