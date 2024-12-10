4eb5698b9b4986bd416e75f73092979dc4bcda1d
mithunpaul08 <mithunpaul08@gmail.com>
2024-12-10 07:33:19 -0700

    adding pytest for yes expose and no expose. todo. the second test case shouldnt pass as of now, since we still dont know what the ideal value is. so check. also merge this carefully with main. the other branch sst2_classical_2 had some new command line args added

--- test_oov_no_pair.py
+++ test_oov_no_pair.py
@@ -13,8 +13,29 @@ import classify
 #         assert round(smart_acc,1)  <= 0.9 
 
 
-#for a combination of train=20,val=10,test=10
+#for a combination of train=20,val=10,test=10+exposing val data during initialization of model 1
 def test_sst2_classical1(monkeypatch):        
+        monkeypatch.setattr(sys, 'argv', 
+                            ['classify.py', 
+                             '--dataset', 'sst2',
+                             '--parser', 'Spider',
+                             '--ansatz', 'SpiderAnsatz',
+                             '--model14type', 'PytorchModel',
+                             '--trainer', 'PytorchTrainer',
+                             '--epochs_train_model1', '7',
+                             '--no_of_training_data_points_to_use', '20',
+                             '--no_of_val_data_points_to_use', '10',
+                             '--expose_model1_val_during_model_initialization'                             
+                             ])
+        model4_loss, model4_acc=classify.main()                         
+        assert round(model4_loss,2)  >= 0.6
+        assert round(model4_loss,2)  <= 0.7
+        assert round(model4_acc,1)  >= 0.5 
+        assert round(model4_acc,1)  <= 0.6 
+
+
+#for a combination of train=20,val=10,test=10 +not exposing val data during initialization of model 1
+def test_sst2_classical1_no_expose(monkeypatch):        
         monkeypatch.setattr(sys, 'argv', 
                             ['classify.py', 
                              '--dataset', 'sst2',
